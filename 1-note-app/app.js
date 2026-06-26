c9a6d0d8c64bced37bbccfcc0d01cd29ffb9cb37
Romano-Mancini <romanomancini03@gmail.com>
2026-06-26 19:29:34 +0200

    feat: added yargs parsing

--- 1-note-app/app.js
+++ 1-note-app/app.js
@@ -1,10 +1,41 @@
-const fs = require('fs')
+const { version } = require('./package.json')
+
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
-const validator = require('validator')
+const yargs = require('yargs')
 
-fs.appendFileSync('notes.txt', 'Title.\n')
-fs.appendFileSync('notes.txt', 'Description.\n')
-fs.appendFileSync('notes.txt', 'Paragraph.')
+yargs.version = version
 
-console.log(getNotes())
-console.log(validator.isEmail("john@example.com"))
+yargs.command({
+    command: "add",
+    describe: "adds a note",
+    handler: function () {
+        console.log("Adding a note!")
+    }
+})
+
+yargs.command({
+    command: "remove",
+    describe: "removes a note",
+    handler: function () {
+        console.log("Removing a note!")
+    }
+})
+
+yargs.command({
+    command: "list",
+    describe: "lists all notes",
+    handler: function () {
+        console.log("Listing notes!")
+    }
+})
+
+yargs.command({
+    command: "read",
+    describe: "reads a note",
+    handler: function () {
+        console.log("Reading a note!")
+    }
+})
+
+yargs.parse()

0ef64ad2df973e6b70f509d56b6f8791de81052f
Romano-Mancini <romanomancini03@gmail.com>
2026-06-26 13:33:54 +0200

    feat: added validator

--- 1-note-app/app.js
+++ 1-note-app/app.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const getNotes = require('./notes.js')
+const validator = require('validator')
 
 fs.appendFileSync('notes.txt', 'Title.\n')
 fs.appendFileSync('notes.txt', 'Description.\n')
 fs.appendFileSync('notes.txt', 'Paragraph.')
 
 console.log(getNotes())
+console.log(validator.isEmail("john@example.com"))

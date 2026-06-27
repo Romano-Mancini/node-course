a8092c01bdd845807501bd36f3130eb31dbabc82
Romano-Mancini <romanomancini03@gmail.com>
2026-06-27 14:04:20 +0200

    feat: added first support for weather

--- 2-weather-app/app.js
+++ 2-weather-app/app.js
@@ -1,2 +1,13 @@
 require("dotenv").config();
+const request = require("postman-request");
 const apiKey = process.env.API_KEY;
+
+const URL =
+  "http://api.weatherstack.com/current?access_key=" +
+  apiKey +
+  "&query=New York";
+
+request({ url: URL }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current.temperature);
+});

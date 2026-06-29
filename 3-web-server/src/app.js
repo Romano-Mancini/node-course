4af87bced0393de4fc82698ef963c80df1434f53
Romano-Mancini <romanomancini03@gmail.com>
2026-06-29 16:41:16 +0200

    feat: added static assets

--- 3-web-server/src/app.js
+++ 3-web-server/src/app.js
@@ -1,23 +1,24 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello Express!");
-});
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.send({ name: "Romano", surname: "Mancini", age: 22 });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send("<h1>About page</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather page");
+  res.send({ forecast: "It's 30 degrees.", location: "Philadelphia." });
 });
+
 // just displaying when the application is first run,
-// never returned to a user.
+// never returned to the user.
 app.listen(3000, () => {
   console.log("Server is up on port 3000!");
 });

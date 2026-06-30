971c179e7ebcceee793b9094f989ea68d050ebfb
Romano-Mancini <romanomancini03@gmail.com>
2026-06-30 18:47:49 +0200

    feat: added CSS support

--- 3-web-server/public/js/app.js
+++ 3-web-server/public/js/app.js
@@ -2,18 +2,24 @@ console.log("Client side Javascript script is loaded.");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault(); // prevents default reload on submission
   const location = search.value;
 
+  messageOne.textContent = "Loading...";
+
   fetch("http://localhost:3000/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          messageOne.textContent = data.error;
+          messageTwo.textContent = "";
         } else {
-          console.log(data.forecast, data.location);
+          messageOne.textContent = "";
+          messageTwo.textContent = data.forecast + "\n" + data.location;
         }
       });
     },
